75f4d7c27a561a62d57dcd15c8c7c765b4977c5b
rubberyuzu <yuzu0125@gmail.com>
2014-12-13 16:01:12 +0900

    input output success

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import serial
 import time
 
-ser = serial.Serial(
+# initialize start
+
+input_ser = serial.Serial(
 	port = '/dev/tty.Bluetooth-Incoming-Port',
 	baudrate = 19200,
 	parity = serial.PARITY_NONE,
@@ -11,8 +13,31 @@ ser = serial.Serial(
 	xonxoff = 0,
 	rtscts = 0,
 	)
-# ser.open()
-# ser.isOpen()
+input_ser.close()
+input_ser.open()
+
+
+output_ser = serial.Serial(
+	port = '/dev/tty.usbmodemfd121',
+	baudrate = 9600,
+	parity = serial.PARITY_NONE,
+	bytesize = serial.EIGHTBITS,
+	stopbits = serial.STOPBITS_ONE,
+	timeout = None,
+	xonxoff = 0,
+	rtscts = 0,
+	)
+output_ser.close()
+output_ser.open()
+
+# initialize over
 
 while 1:
-	print ser.readline()
+	# print input_ser.readline()
+	if "yuzu" in input_ser.readline():
+		print 'A'
+		output_ser.write('A\n')
+
+	else:
+		print "yuzu not found!"
+		output_ser.write('a\n')
