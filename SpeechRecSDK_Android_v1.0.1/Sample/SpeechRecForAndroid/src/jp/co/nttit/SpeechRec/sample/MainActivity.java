555a52e46134313814d0379769144660a2d1fb06
Yuki Takeda <yuki@imaginaryshort.com>
2014-12-14 18:22:02 +0900

    modify SpeechRec sample

--- SpeechRecSDK_Android_v1.0.1/Sample/SpeechRecForAndroid/src/jp/co/nttit/SpeechRec/sample/MainActivity.java
+++ SpeechRecSDK_Android_v1.0.1/Sample/SpeechRecForAndroid/src/jp/co/nttit/SpeechRec/sample/MainActivity.java
@@ -3,8 +3,11 @@
  */
 package jp.co.nttit.SpeechRec.sample;
 
+import java.util.HashMap;
+
 import jp.co.nttit.SpeechRec.sample.R;
 import android.os.Bundle;
+import android.R.integer;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
@@ -17,57 +20,87 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-	private Button buttonStart;
-	private TextView textResult = null;
-	private RadioButton radioHigh;
-	private RadioButton radioLow;
-	private String mResultString = "";
+    private Button buttonStart;
+    private TextView textResult = null;
+    private RadioButton radioHigh;
+    private RadioButton radioLow;
+    private String mResultString = "";
+    private HashMap<String, String> wordMap;
 
-	/** 音声認識アクティビティのリクエストID */
-	private static final int RECOGNIZE_ACTIVITY_REQUEST_ID = 1;
+    /** 音声認識アクティビティのリクエストID */
+    private static final int RECOGNIZE_ACTIVITY_REQUEST_ID = 1;
 
-	@SuppressLint("InlinedApi")
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
-		setContentView(R.layout.main);
-		buttonStart = (Button) findViewById(R.id.button_start);
-		buttonStart.setEnabled(false);
-		textResult = (TextView) findViewById(R.id.text_result);
-		radioHigh = (RadioButton) findViewById(R.id.radio_high);
-		radioLow = (RadioButton) findViewById(R.id.radio_low);
+    @SuppressLint("InlinedApi")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        setContentView(R.layout.main);
+        buttonStart = (Button) findViewById(R.id.button_start);
+        buttonStart.setEnabled(false);
+        textResult = (TextView) findViewById(R.id.text_result);
+        radioHigh = (RadioButton) findViewById(R.id.radio_high);
+        radioLow = (RadioButton) findViewById(R.id.radio_low);
 
-		textResult.setTextSize(28.0f);
+        textResult.setTextSize(28.0f);
 
-		buttonStart.setEnabled(true);
-		radioHigh.setEnabled(true);
-		radioLow.setEnabled(true);
-	}
+        buttonStart.setEnabled(true);
+        radioHigh.setEnabled(true);
+        radioLow.setEnabled(true);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+        wordMap = new HashMap<String, String>();
+        wordMap.put("です", "green");
+        wordMap.put("ます", "green");
+        
+        wordMap.put("ゆうき", "pink");
+        
+        wordMap.put("りさちゃん", "orange");
+        
+        wordMap.put("酔っちゃった", "blue");
+        wordMap.put("酔っちゃった", "blue");
+        
+        
 
-	@Override
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		switch (requestCode) {
-		case RECOGNIZE_ACTIVITY_REQUEST_ID:
-			mResultString = data.getStringExtra((resultCode == RESULT_OK) ? "replace_key" : "error_message");
-			textResult.setText(mResultString);
-			break;
-		}
-	}
+    }
 
-	public void onClickStart(final View view) {
-		int sbm_mode = radioHigh.isChecked() ? 0 : 1;
-		Intent intent = new Intent(this, RecognitionActivity.class);
-		intent.putExtra(RecognitionActivity.KEY_SBM_MODE, sbm_mode);
-		// 別途発行されるAPIキーを設定してください(以下の値はダミーです)
-		intent.putExtra(RecognitionActivity.KEY_API_KEY, "123456789");
-		startActivityForResult(intent, RECOGNIZE_ACTIVITY_REQUEST_ID);
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+        case RECOGNIZE_ACTIVITY_REQUEST_ID:
+            mResultString = data
+                    .getStringExtra((resultCode == RESULT_OK) ? "replace_key"
+                            : "error_message");
+
+            
+            textResult.setText(mResultString);
+            for (String key : wordMap.keySet()) {
+                if (mResultString.indexOf(key) != -1) {
+                    
+                    //wordMap.get(key)
+                    
+                    
+                    
+                }
+            }
+            break;
+        }
+    }
+
+    public void onClickStart(final View view) {
+        int sbm_mode = radioHigh.isChecked() ? 0 : 1;
+        Intent intent = new Intent(this, RecognitionActivity.class);
+        intent.putExtra(RecognitionActivity.KEY_SBM_MODE, sbm_mode);
+        // 別途発行されるAPIキーを設定してください(以下の値はダミーです)
+        intent.putExtra(
+                RecognitionActivity.KEY_API_KEY,
+                "72634749316e75537a6433305371576862375a367759616e514a674e2f48706e5331445a2e442e4b434431");
+        startActivityForResult(intent, RECOGNIZE_ACTIVITY_REQUEST_ID);
+    }
 }
